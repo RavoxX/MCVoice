37103b356c07b77b0fa33d7c60352edf03cb92a7
Jannik Wappler <ravoxx.wappler123@gmail.com>
2026-09-25 10:43:30 +0200

    build: generate minecraft/ for 1.8.1 from 5749882

---
+++ minecraft/legacyfabric/src-gen/java/dev/mcvoice/platform/mc/McLogging.java
@@ -0,0 +1,33 @@
+package dev.mcvoice.platform.mc;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import dev.mcvoice.client.log.Category;
+import dev.mcvoice.client.log.LogSink;
+
+/** Bridges MCVoice log categories to the game's Log4j logging. */
+public final class McLogging implements LogSink {
+    private static final Logger LOG = LogManager.getLogger("MCVoice");
+
+    @Override
+    public void log(Level level, Category category, String message, Throwable error) {
+        String m = "[" + category + "] " + message;
+        switch (level) {
+            case TRACE:
+                LOG.trace(m, error);
+                break;
+            case DEBUG:
+                LOG.debug(m, error);
+                break;
+            case INFO:
+                LOG.info(m, error);
+                break;
+            case WARN:
+                LOG.warn(m, error);
+                break;
+            default:
+                LOG.error(m, error);
+        }
+    }
+}
