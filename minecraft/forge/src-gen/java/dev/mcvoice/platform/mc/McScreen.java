32d928f3fee09ed41087f332e6ce22bb87d0d1f9
Jannik Wappler <ravoxx.wappler123@gmail.com>
2026-09-25 11:25:20 +0200

    build: generate minecraft/ for 1.19.1 from 4adf2c2 [skip ci]

--- minecraft/forge/src-gen/java/dev/mcvoice/platform/mc/McScreen.java
+++ minecraft/forge/src-gen/java/dev/mcvoice/platform/mc/McScreen.java
@@ -41,7 +41,13 @@ public final class McScreen extends Screen {
 
     public void render(PoseStack g, int mouseX, int mouseY, float partialTicks) {
 
+
+
+
         ui.render(new McCanvas(g), mouseX, mouseY, partialTicks);
+
+
+
     }
 
 
