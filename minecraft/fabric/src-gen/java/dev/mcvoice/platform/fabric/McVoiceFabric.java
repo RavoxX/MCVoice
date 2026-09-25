e26996e982ca5efe82f075f374c1d36bd9239601
Jannik Wappler <ravoxx.wappler123@gmail.com>
2026-09-25 11:25:39 +0200

    build: generate minecraft/ for 1.20.6 from 4adf2c2 [skip ci]

--- minecraft/fabric/src-gen/java/dev/mcvoice/platform/fabric/McVoiceFabric.java
+++ minecraft/fabric/src-gen/java/dev/mcvoice/platform/fabric/McVoiceFabric.java
@@ -76,10 +76,23 @@ public final class McVoiceFabric implements ClientModInitializer {
 
         HudRenderCallback.EVENT.register((graphics, delta) -> client.renderHud(new McCanvas(graphics)));
 
+
+
+
+
         ClientPlayConnectionEvents.DISCONNECT.register((handler, mc) -> client.invalidateWorld("disconnect"));
         ClientPlayConnectionEvents.JOIN.register((handler, sender, mc) -> client.invalidateWorld("join_world"));
         ClientLifecycleEvents.CLIENT_STOPPING.register(mc -> client.shutdown());
     }
+
+
+
+
+
+
+
+
+
 
     /** Plugin channels for the Simple Voice Chat compatibility layer via Fabric networking. */
     static final class FabricSvcChannels implements SimpleVoiceChatAdapter {
