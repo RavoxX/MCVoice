2476f30a515ba9ef014e3f5150ff5337b05be122
Jannik Wappler <ravoxx.wappler123@gmail.com>
2026-09-25 10:43:41 +0200

    build: generate minecraft/ for 1.8.7 from 5749882

---
+++ minecraft/legacyfabric/src-gen/java/dev/mcvoice/platform/fabric/KeyHolder.java
@@ -0,0 +1,9 @@
+
+package dev.mcvoice.platform.fabric;
+
+/** Implemented on GameOptions by {@link dev.mcvoice.platform.fabric.mixin.GameOptionsMixin}. */
+public interface KeyHolder {
+    /** Appends the MCVoice key bindings to GameOptions#allKeys once; {@code reload} re-reads options.txt. */
+    void mcvoice$addKeys(boolean reload);
+}
+
